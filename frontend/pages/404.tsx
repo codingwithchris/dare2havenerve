fd16e59beb9d9e121fb204d99e8a94402e5553d2
Chris Hahn <chris+digital@chrishahn.me>
2020-10-06 22:16:43 -0400

    Add Subpages and 404

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,16 @@
+import { Box, Container, Typography } from '@material-ui/core';
+
+import { Page } from '@/components';
+
+export default function NotFound() {
+    return (
+        <Page
+            metaTitle="Page not found"
+            metaDescription="Say something cool here"
+        >
+            <Box className="app-content" py={20}>
+                Page Not Found
+            </Box>
+        </Page>
+    );
+}
