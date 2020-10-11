1c736fd6893ac4c677e96c76d48788932546e95f
Chris Hahn <chris+digital@chrishahn.me>
2020-10-11 16:29:06 -0400

    No Typechecking For Donate Form

--- frontend/src/components/DonateForm/DonateForm.js
+++ frontend/src/components/DonateForm/DonateForm.js
@@ -1,18 +1,12 @@
 import Head from 'next/head';
-import { isSSR } from '@/lib/ssr';
+import { isSSR } from '../../__lib__/ssr';
 import styles from './DonateForm.module.css';
 
-export const DonateForm: React.FC = () => {
+// In JS to avoid typechecking
+export const DonateForm = () => {
     return (
         <>
-            <Head>
-                {!isSSR && (
-                    <script
-                        src="https://donorbox.org/widget.js"
-                        paypalExpress="true"
-                    />
-                )}
-            </Head>
+            <Head>{!isSSR && <script src="https://donorbox.org/widget.js" paypalExpress="true" />}</Head>
             <div className={styles.donateForm}>
                 <iframe
                     title="donate"
@@ -20,9 +14,9 @@ export const DonateForm: React.FC = () => {
                     frameBorder="0"
                     height="900px"
                     name="donorbox"
-                    scrolling="no"
                     seamless="seamless"
-                    src="https://donorbox.org/embed/dare2havenerve-fundraiser?donation_meter_color=%2341a2d8"
+                    scrolling="no"
+                    src="https://donorbox.org/embed/dare2havenerve-fundraiser"
                     style={{
                         maxWidth: '425px',
                         minWidth: '250px',
--- frontend/src/components/GoalMeter/GoalMeter.js
+++ frontend/src/components/GoalMeter/GoalMeter.js
@@ -1,12 +1,8 @@
 import Head from 'next/head';
-import { isSSR } from '@/lib/ssr';
+import { isSSR } from '../../__lib__/ssr';
 
 export const GoalMeter = () => {
-    <Head>
-        {!isSSR && (
-            <script src="https://donorbox.org/widget.js" paypalExpress="true" />
-        )}
-    </Head>;
+    <Head>{!isSSR && <script src="https://donorbox.org/widget.js" paypalExpress="true" />}</Head>;
 
     return (
         <div className="">
@@ -17,7 +13,7 @@ export const GoalMeter = () => {
                 name="donorbox"
                 scrolling="no"
                 seamless="seamless"
-                src="https://donorbox.org/embed/dare2havenerve-fundraiser?donation_meter_color=%23FF5349&amp;only_donation_meter=true"
+                src="https://donorbox.org/embed/dare2havenerve-fundraiser?donation_meter_color=%23FF5349&amp;only_donation_meter=true" // eslint-disable-line
                 style={{
                     width: '100%',
                     minWidth: '250px',
