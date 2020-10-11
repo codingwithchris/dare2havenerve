28e5d44ac5314f5ac402e709e49dc94788e6eb32
Chris Hahn <chris+digital@chrishahn.me>
2020-10-11 17:03:29 -0400

    Remove ENV.. Oi Vey… How Did This Happen

--- frontend/src/__lib__/client.ts
+++ frontend/src/__lib__/client.ts
@@ -4,5 +4,4 @@ export default sanityClient({
     projectId: 'hr6pb86u', // you can find this in sanity.json
     dataset: 'production', // or the name you chose in step 1
     useCdn: false, // `false` if you want to ensure fresh data
-    token: process.env.SANITY_TOKEN,
 });
